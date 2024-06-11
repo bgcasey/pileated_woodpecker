c31191ff5715d8fca412ad2efa9489aa17906d54
Your Name <brendangerardcasey@gmail.com>
2024-06-11 13:46:17 -0600

    cleaned and commented code.

---
+++ 1_code/r_scripts/03_create_spatial_object_with_point_count_locations.R
@@ -0,0 +1,46 @@
+# ---
+# title: "Create spatial object with point count locations"
+# author: "Brendan Casey"
+# created: "2024-02-06"
+# description: "This script generates shapefiles for point count 
+# locations "
+# ---
+
+# 1. Setup ----
+
+## 1.1 Load packages ----
+library(sf) # for handling and analyzing spatial data
+library(tidyverse) # data manipulation and visualization
+
+## 1.2 Import data ----
+# Load the cleaned Wildtrax data
+load("0_data/manual/wildtrax_cleaned_piwo_2024-06-11.rData")
+
+# 2. Get XY coordinates ----
+
+## 2.1 Create spatial object ----
+# Generate a spatial object with unique point count locations
+ss_xy <- wildtrax_cleaned_piwo %>%
+  ungroup() %>%
+  dplyr::select(location, lon, lat) %>%
+  distinct()
+
+# Convert the data to a spatial object with CRS set to 
+# EPSG:4326 (WGS84)
+ss_xy_4326 <- ss_xy %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = FALSE)
+
+## 2.2 Transform coordinates ----
+# Convert the coordinates to meters (EPSG:3857)
+ss_xy_3857 <- ss_xy_4326 %>%
+  st_transform(crs = 3857) %>%
+  dplyr::mutate(x_3857 = sf::st_coordinates(.)[,1],
+                y_3857 = sf::st_coordinates(.)[,2]) %>%
+  dplyr::select(-c(lon, lat))
+
+# 3. Save ----
+save(ss_xy_4326, file = "0_data/manual/spatial/ss_xy_4326.rData")
+st_write(ss_xy_4326, "0_data/manual/spatial/ss_xy_4326.shp")
+
+save(ss_xy_3857, file = "0_data/manual/spatial/ss_xy_3857.rData")
+st_write(ss_xy_3857, "0_data/manual/spatial/ss_xy_3857.shp")
