e688ffbe18199169d8da40a5e1fcf8080ba57adf
bgcasey <bgcasey@ualberta.ca>
2024-08-05 06:44:07 -0600

    added dynamic save paths

--- 1_code/r_scripts/boosted_regression_trees.R
+++ 1_code/r_scripts/boosted_regression_trees.R
@@ -46,6 +46,9 @@ random_data <- data_brt[random_index, ]
 o <- random_data$PIWO_offset
 random_data <- dplyr::select(random_data, -c(PIWO_offset))
 
+## 1.7. Set save file path ----
+path <- "3_output/models/all_vars"
+
 # 2. Boosted Regression Tree ----
 ## 2.1. Apply `dismo::gbm.step` to tuned parameters ----
 brt_1 <- gbm.step(
@@ -63,18 +66,25 @@ brt_1 <- gbm.step(
   plot.main = FALSE
 )
 
-save(brt_1, file = "3_output/models/brt_1.rData")
+save(brt_1,
+  file = paste0(path, "/brt_1.rData")
+)
 
 ## 2.2. Get model stats ----
 brt_1_stats <- calculate_brt_stats(model = brt_1) %>%
   rename(auc = discrimination.mean)
 
-save(brt_1_stats, file = "3_output/model_results/brt_1_stats.rData")
+save(brt_1_stats,
+  file = paste0(path, "/brt_1_stats.rData")
+)
 
 # 3. Simplified Boosted Regression Tree ----
 ## 3.1. Drop variables that don't improve model performance ----
 brt_1_simp <- gbm.simplify(brt_1, plot = FALSE)
-save(brt_1_simp, file = "2_pipeline/store/models/brt_1_simp.rData")
+
+save(brt_1_simp,
+  file = paste0(path, "/brt_1_simp.rData")
+)
 
 ## 3.2. Remove non-numeric characters from the row names ----
 rownames(brt_1_simp$deviance.summary) <-
@@ -91,6 +101,7 @@ random_data_simp <- random_data %>%
 
 ## 3.5. Re tune model ----
 # Re tune model with reduced number of predictors using tune_models.R.
+load("2_pipeline/store/tuned_param_2.rData")
 
 ## 3.6 BRT Second Iteration ----
 brt_2 <- gbm.step(
@@ -99,20 +110,25 @@ brt_2 <- gbm.step(
   gbm.y = 1,
   offset = o,
   family = "bernoulli",
-  tree.complexity = tuned_param$interaction.depth[1],
-  n.minobsinnode = tuned_param$n.minobsinnode[1],
-  learning.rate = tuned_param$shrinkage[1],
-  bag.fraction = tuned_param$bag.fraction[1],
+  tree.complexity = tuned_param_2$interaction.depth[1],
+  n.minobsinnode = tuned_param_2$n.minobsinnode[1],
+  learning.rate = tuned_param_2$shrinkage[1],
+  bag.fraction = tuned_param_2$bag.fraction[1],
   cv.folds = 10,
   silent = FALSE
 )
 
-save(brt_2, file = "2_pipeline/store/models/brt_2.rData")
+save(brt_2,
+  file = paste0(path, "/brt_2.rData")
+)
 
 ## 3.7. Get model stats ----
 brt_2_stats <- calculate_brt_stats(model = brt_2) %>%
   rename(auc = discrimination.mean)
-save(brt_2_stats, file = "3_output/model_results/brt_2_stats.rData")
+
+save(brt_2_stats,
+  file = paste0(path, "/brt_2_stats.rData")
+)
 
 # 4. Bootstrap model ----
 # Perform bootstrap iterations through the simplified
@@ -136,13 +152,13 @@ save(brt_2_stats, file = "3_output/model_results/brt_2_stats.rData")
 
 bootstrap_brt <- function(data, n_iterations) {
   # Initialize an empty dataframe for stats
-  
+
   # Number of predictors in random_data
   num_predictors <- ncol(random_data) - 1
-  
+
   # Create the column names for covariates dynamically
   cov_columns <- paste0("cov_", 1:num_predictors)
-  
+
   # Create the data frame with dynamic covariate columns
   all_stats_df <- data.frame(
     model = character(),
@@ -155,7 +171,7 @@ bootstrap_brt <- function(data, n_iterations) {
     predict_AUC = numeric(),
     stringsAsFactors = FALSE
   )
-  
+
   # Add covariate columns
   for (cov in cov_columns) {
     all_stats_df[[cov]] <- numeric()
@@ -179,7 +195,7 @@ bootstrap_brt <- function(data, n_iterations) {
 
     o_test <- test_data$PIWO_offset
     test_data <- dplyr::select(test_data, -PIWO_offset)
-    
+
     # Fit the model
     model <- gbm.step(
       data = train_data,
@@ -187,10 +203,10 @@ bootstrap_brt <- function(data, n_iterations) {
       gbm.y = 1,
       offset = o_train,
       family = "bernoulli",
-      tree.complexity = tuned_param_max$interaction.depth[1],
-      n.minobsinnode = tuned_param_max$n.minobsinnode[1],
-      learning.rate = tuned_param_max$shrinkage[1],
-      bag.fraction = tuned_param_max$bag.fraction[1],
+      tree.complexity = tuned_param_2$interaction.depth[1],
+      n.minobsinnode = tuned_param_2$n.minobsinnode[1],
+      learning.rate = tuned_param_2$shrinkage[1],
+      bag.fraction = tuned_param_2$bag.fraction[1],
       silent = FALSE,
       plot.main = FALSE
     )
@@ -233,9 +249,10 @@ bootstrap_brt <- function(data, n_iterations) {
 n_iterations <- 50
 
 # run the bootstrap function
+random_data_simp$PIWO_offset <- o
 bootstrap_models <- bootstrap_brt(random_data_simp, n_iterations)
 
 ## 4.3. Save models ----
 save(bootstrap_models,
-  file = "3_output/model_results/bootstrap_models.rData"
+  file = paste0(path, "/bootstrap_models.rData")
 )
