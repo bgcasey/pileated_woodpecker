a3d0368719ab3a71a92995d1496e1c551b5cb964
bgcasey <bgcasey@ualberta.ca>
2024-08-09 01:59:04 -0600

    cleaned

--- 1_code/r_scripts/functions/mosaic_rasters.R
+++ 1_code/r_scripts/functions/mosaic_rasters.R
@@ -2,10 +2,10 @@
 # title: "Mosaic rasters downloaded from earth engine"
 # author: "Brendan Casey"
 # created: "2024-08-05"
-# description: 
-#   This script creates a mosaic of raster files downloaded from 
-#   Google Earth Engine. It includes steps to list directories, 
-#   load raster files, and generate a mean mosaic of these rasters. 
+# description:
+#   This script creates a mosaic of raster files downloaded from
+#   Google Earth Engine. It includes steps to list directories,
+#   load raster files, and generate a mean mosaic of these rasters.
 # ---
 
 # 1. Setup ----
@@ -16,23 +16,23 @@ library(tidyverse) # data manipulation and visualization
 library(sf) # handling spatial vector data
 
 ## 1.2 Import data ----
-# Load your data here
-# Example:
-# study_area <- st_read("0_data/external/Alberta/alberta.shp")
+aoi <- st_read("0_data/external/alberta/Alberta.shp")
 
 # 2. Mosaic rasters ----
-# This section creates a mosaic of raster files from a list of .tif 
+# This section creates a mosaic of raster files from a list of .tif
 # files in a directory.
 
 ## 2.1 List directories ----
-path_name <- paste0("/Users/brendancasey/Library/CloudStorage/",
-                    "GoogleDrive-bgcasey@ualberta.ca/My Drive/",
-                    "gee_exports/")
+path_name <- paste0(
+  "/Users/brendancasey/Library/CloudStorage/",
+  "GoogleDrive-bgcasey@ualberta.ca/My Drive/",
+  "gee_exports/"
+)
 
 fl <- list.files(
-  path_name, 
-  pattern = 'focal_image_500.*\\.tif$', 
-  recursive = TRUE, 
+  path_name,
+  pattern = "focal_image_500.*\\.tif$",
+  recursive = TRUE,
   full.names = TRUE
 )
 
@@ -40,13 +40,13 @@ fl <- list.files(
 rasters <- terra::sprc(fl)
 m <- terra::mosaic(rasters, fun = "mean")
 
+## 2.3 Crop to aoi ----
+aoi_tr <- st_transform(aoi, st_crs(m))
+m <- mask(m, aoi_tr)
+
 ## 2.3 Save as .tif ----
 writeRaster(
-  m, 
-  filename = "0_data/manual/predictor/gee/focal_image_500.tif",  
+  m,
+  filename = "0_data/manual/predictor/gee/focal_image_500.tif",
   overwrite = TRUE
 )
-
-
-
-
