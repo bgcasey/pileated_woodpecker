b20f73e6094a9e12654ce4e9299a0b966ab97911
bgcasey <bgcasey@ualberta.ca>
2024-08-20 13:39:36 -0600

    summarize gbm model results

--- 1_code/r_scripts/summarize_results.R
+++ 1_code/r_scripts/summarize_results.R
@@ -1,125 +1,142 @@
 # ---
-# title: "6_summarize_results
+# title: "Summarize Results"
 # author: "Brendan Casey"
 # created: "2023-12-01"
+# description: 
+#   "This script summarizes gbm model results. It includes steps to 
+#   plot variable importance and generate partial dependence plots. 
+#   The script uses bootstrapped model statistics and a GBM model. 
+#   The final output is a set of plots saved as PNG files."
 # ---
 
-##////////////////////////////////////////////////////////////////
-
-#Setup ----
-
-##Load packages----
-library(tidyverse)
-library(dismo)
+# 1. Setup ----
+## 1.1 Load packages ----
+library(tidyverse) # Data manipulation 
+library(ggplot2) #visualization
+# Boosted Regression trees
 library(gbm)
-##///////////
+# Arranging multiple plots
+library(gridExtra)
+library(grid)
 
-##Load BRTs
-load("3_output/models/WT/brt_nfis_ls_hlc_terrain_canopy_29.rData")
+## 1.2 Set file path ----
+path <- "3_output/models/s2_noOff_noYear"
 
-##////////////////////////////////////////////////////////////////
-# plot variable importance----
+## 1.3 Load data ----
+# Bootstrapped models
+load(paste0(path, "/bootstrap_models_stats.rData"))
+load(paste0(path, "/brt_3.rData"))
 
-##////////////
-## brt_nfis_ls_hlc_terrain_canopy_29----
+# 2. Plot variable importance ----
+# Plot the variable importance from the bootstrapped
+# models.
 
-data<-summary(brt_nfis_ls_hlc_terrain_canopy_29)
+## 2.1 Convert data from wide to long format ----
+bootstrap_models_stats <- bootstrap_models_stats %>%
+  rownames_to_column(var = "Metric") 
 
-## Take the top 10 predictors
-top_data <- head(data[order(-data$rel.inf), ], 15)
+## 2.2 Separate means and standard deviations ----
+means <- bootstrap_models_stats %>%
+  filter(Metric == "mean")
 
-## Remove "mean_" from the "var" column
-top_data$var <- gsub("mean_", "", top_data$var)
-top_data$var <- gsub("lat", "latitude", top_data$var)
-top_data$var <- gsub("FirstReturns.H150", "Height_150", top_data$var)
-top_data$var <- gsub("Biomass.H150", "Biomass_150", top_data$var)
-top_data$var <- gsub("`Exposed_Barren land_1000`", "Exposed_Barren land_1000", top_data$var)
+sds <- bootstrap_models_stats %>%
+  filter(Metric == "sd")
 
+## 2.3 Merge means and standard deviations ----
+importance_df <- means %>%
+  gather(key = "Variable", value = "Importance", -Metric) %>%
+  dplyr::select(-Metric) %>%
+  left_join(sds %>% 
+              gather(key = "Variable", value = "SD", -Metric) %>% 
+              dplyr::select(-Metric),
+            by = "Variable") %>%
+  slice(-1:-6) %>%
+  mutate(Variable = str_replace_all(Variable, c("ls_" = "",
+                                                "s2_" = "",
+                                                "_mean_500" = "")))
 
-# top_data<-top_data%>%dplyr::filter(var!="latitude")
-top_data<-top_data%>%dplyr::filter(var!="lon")
-top_data$var <- gsub("DRS", "NDRS", top_data$var)
-
-
-## create a bar plot
-bar_plot <- ggplot(top_data, aes(x = rel.inf, y = reorder(var, rel.inf))) +
-  geom_bar(stat = "identity", fill = "#616a6b") +
+## 2.4 Generate bar plot with error bars ----
+variable_importance_plot <- ggplot(importance_df, 
+                                   aes(x = reorder(Variable, 
+                                                   Importance), 
+                                       y = Importance)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_errorbar(aes(ymin = Importance - SD, ymax = Importance + SD), 
+                width = 0.2) +
+  coord_flip() +  # Flip coordinates for readability
+  theme_minimal(base_size = 14) +  # Set a base font size
   labs(
-       y = "Predictor",
-       x = "Relative influence")+
-  theme_minimal()+
-  theme(panel.grid = element_blank(),  panel.background = element_rect(color = "black", fill = "white"))
+    x = "Predictor Variables",
+    y = "Variable Importance") +
+  theme(
+    panel.grid.major = element_blank(),  # Remove major gridlines
+    panel.grid.minor = element_blank(),  # Remove minor gridlines
+    axis.text = element_text(size = 12, color = "black"),  
+    axis.title = element_text(size = 14, face = "bold"),  
+    plot.title = element_text(size = 16, face = "bold", hjust = 0.5),  
+    plot.margin = margin(10, 10, 10, 10),  # Adjust plot margins
+    panel.border = element_blank(),  # Remove panel border
+    axis.line = element_line(color = "black")  # Add axis lines
+  )
 
-ggsave(filename = "3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_bar.png", plot = bar_plot, width=8, height=4, units="in", dpi=300)
+ggsave(paste0(path, "/variable_importance_plot.png"), 
+       plot = variable_importance_plot, width = 10, height = 8, 
+       dpi = 300)
 
-##////////////////////////////////////////////////////////////////
-### Partial dependence plots----
+# 3. Partial dependence plots ----
+# Generate partial dependence plots for the top 6
+# variables sorted by importance. It includes steps to extract variable
+# importance, generate partial dependence plots, and arrange them in
+# a grid with a common y-axis label.
 
-#### Individual plots----
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_latitude.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 1, smooth=TRUE,  x.label= "latitude", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
+## 3.1 Extract variable importance ----
+model <- brt_3
+variable_importance <- summary(model, plotit = FALSE)
+top_variables <- head(variable_importance$var, 6)
 
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_TPI_5000.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 81, smooth=TRUE, x.label= "TPI_5000", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
+## 3.2 Generate partial dependence plots ----
+plot_list <- list()
 
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_Water_1000.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 28, smooth=TRUE, x.label= "Water_1000", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
+for (var in top_variables) {
+  temp_plot <- plot.gbm(model, i.var = var, return.grid = TRUE, 
+                        smooth = TRUE, type= "response")
+  
+  if (is.factor(temp_plot[[1]]) || is.character(temp_plot[[1]])) {
+    temp_plot[[1]] <- as.factor(temp_plot[[1]])
+    p <- ggplot(temp_plot, aes_string(x = names(temp_plot)[1], y = "y")) +
+      geom_bar(stat = "identity") +
+      labs(x = str_replace_all(var, c("ls_" = "", "s2_" = "", "_mean_500" = "")), 
+           y = NULL) +  # Remove individual y-axis labels
+      theme_minimal() +
+      theme(panel.grid.major = element_blank(), 
+            panel.grid.minor = element_blank()) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  } else {
+    p <- ggplot(temp_plot, aes_string(x = names(temp_plot)[1], y = "y")) +
+      geom_line() +
+      labs(x = str_replace_all(var, c("ls_" = "", "s2_" = "", "_mean_500" = "")), 
+           y = NULL) +  # Remove individual y-axis labels
+      theme_minimal() +
+      theme(panel.grid.major = element_blank(), 
+            panel.grid.minor = element_blank())
+  }
+  
+  plot_list[[var]] <- p
+}
 
+## 3.3 Arrange plots in a grid ----
+grid_plots <- do.call(grid.arrange, c(plot_list, ncol = 2))
 
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_Height_150.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 94, smooth=TRUE, x.label= "Height_150", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
+## 3.4 Create and save combined grob ----
+combined_grob <- arrangeGrob(grid_plots, 
+                             left = textGrob("Occupancy Probability", 
+                                             rot = 90, 
+                                             gp = gpar(fontsize = 15)))
+ggsave(paste0(path, "/partial_dependence_plots.png"), 
+       plot = combined_grob, width = 12, height = 9)
 
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_Wetland_treed_1000.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 29, smooth=TRUE, x.label= "Wetland_treed_1000", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
-
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_Mixedwood_150.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 8, smooth=TRUE, x.label= "Mixedwood_150", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
-
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_Mixedwood_1000.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 26, smooth=TRUE, x.label= "Mixedwood_1000", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
-
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_Exposed_Barren_land_1000.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 24, smooth=TRUE, x.label= "Exposed_Barren_land_1000", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
-
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_Biomass_150.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 84, smooth=TRUE, x.label= "Biomass_150", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
-
-
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_Biomass_150.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 84, smooth=TRUE, x.label= "Biomass_150", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
-
-
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_DRS_1000.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 55, smooth=TRUE, x.label= "DRS_1000", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
-
-
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_DRS_150.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 35, smooth=TRUE, x.label= "DRS_150", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
-
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_NDRS_150.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 36, smooth=TRUE, x.label= "NDRS_150", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
-
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_NDRS_1000.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 56, smooth=TRUE, x.label= "NDRS_1000", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
-
-
-
-png(file="3_output/model_summaries/brt_nfis_ls_hlc_terrain_canopy_29_TWI_150.png", width=10, height=8, units="in", res=300)
-gbm.plot(brt_nfis_ls_hlc_terrain_canopy_29, variable.no = 61, smooth=TRUE, x.label= "TWI_150", plot.layout = c(1,1), write.title = FALSE,show.contrib = FALSE, cex.lab=1.5)
-dev.off()
+# Print the combined grob
+grid.newpage()
+grid.draw(combined_grob)
 
 
