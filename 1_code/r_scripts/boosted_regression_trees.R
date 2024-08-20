cf74e9654af76f982c5977e1faa300f161780343
bgcasey <bgcasey@ualberta.ca>
2024-08-20 13:43:35 -0600

    create a table that summarizes bootstrapped model stats

--- 1_code/r_scripts/boosted_regression_trees.R
+++ 1_code/r_scripts/boosted_regression_trees.R
@@ -25,8 +25,8 @@ library(foreach)
 library(parallel) # For parallel processing
 
 ## 1.2 Set file directory path ----
-path <- "3_output/models/s2"
-# path <- "3_output/models/s2_noOff"
+# path <- "3_output/models/s2"
+path <- "3_output/models/ls_noOff_noYear"
 # path <- "3_output/models/ls"
 # path <- "3_output/models/ls_noOff"
 
@@ -41,10 +41,13 @@ load("0_data/manual/formatted_for_models/data_for_models.rData")
 
 ## 1.5 Set up data ----
 data_brt <- data_brt %>%
-  dplyr::select(-starts_with("ls_"),
-                -x_3978,
-                -y_3978,
-                -ends_with("_0")) %>%
+  dplyr::select(
+    -starts_with("s2_"),
+    -x_3978,
+    -y_3978,
+    -year,
+    -ends_with("_0")
+  ) %>%
   na.omit() %>%
   filter(nrname != "grassland")
 
@@ -56,30 +59,32 @@ random_index <- sample(1:nrow(data_brt), nrow(data_brt))
 random_data <- data_brt[random_index, ]
 
 ## 1.8 Define offset ----
-o <- log(random_data$survey_effort)
-random_data <- dplyr::select(random_data, -c(PIWO_offset, survey_effort))
-# random_data <- dplyr::select(random_data, -c(PIWO_offset))
+o <- log(random_data$PIWO_offset)
+# random_data <- dplyr::select(random_data, -c(PIWO_offset, survey_effort))
+random_data <- dplyr::select(random_data, -c(PIWO_offset))
 
 # 2. BRT 1 ----
 ## 2.1 Tune parameters ----
 tune_data <- random_data
-tune_data$offset <- o 
-tuned_param_1 <- tune_gbm_model(data = tune_data,
-                                formula = PIWO_occ ~ . +
-                                offset(offset))
+# tune_data$offset <- o
 # tuned_param_1 <- tune_gbm_model(data = tune_data,
-#                                 formula = PIWO_occ ~ .)
+#                                 formula = PIWO_occ ~ . +
+#                                 offset(offset))
+tuned_param_1 <- tune_gbm_model(
+  data = tune_data,
+  formula = PIWO_occ ~ .
+)
 save(tuned_param_1,
-     file = paste0(path, "/tuned_param_1.rData")
+  file = paste0(path, "/tuned_param_1.rData")
 )
 
 ## 2.2 BRT First iteration ----
-# Apply `dismo::gbm.step` to tuned parameters 
+# Apply `dismo::gbm.step` to tuned parameters
 brt_1 <- gbm.step(
   data = random_data,
   gbm.x = 2:ncol(random_data),
   gbm.y = 1,
-  offset = o,
+  # offset = o,
   family = "bernoulli",
   tree.complexity = tuned_param_1$interaction.depth[1],
   n.minobsinnode = tuned_param_1$n.minobsinnode[1],
@@ -107,7 +112,7 @@ save(brt_1_stats,
 
 ## 3.1 Drop variables that don't improve model performance ----
 # Extract the summary of the model
-model_summary <- summary(brt_1)
+model_summary <- summary(brt_1, plotit = FALSE)
 
 # Filter out predictors with relative influence < 1
 important_predictors <- model_summary$var[model_summary$rel.inf >= 1]
@@ -118,14 +123,16 @@ random_data_simp <- random_data[, c("PIWO_occ", important_predictors)]
 ## 3.2. Tune parameters ----
 # Tune model with reduced number of predictors
 tune_data <- random_data_simp
-tune_data$offset <- o 
-tuned_param_2 <- tune_gbm_model(data = tune_data,
-                              formula = PIWO_occ ~ . +
-                                offset(offset))
+# tune_data$offset <- o
 # tuned_param_2 <- tune_gbm_model(data = tune_data,
-#                                 formula = PIWO_occ ~ .)
+#                               formula = PIWO_occ ~ . +
+#                                 offset(offset))
+tuned_param_2 <- tune_gbm_model(
+  data = tune_data,
+  formula = PIWO_occ ~ .
+)
 save(tuned_param_2,
-     file = paste0(path, "/tuned_param_2.rData")
+  file = paste0(path, "/tuned_param_2.rData")
 )
 
 ## 3.3 BRT Second Iteration ----
@@ -133,18 +140,19 @@ brt_2 <- gbm.step(
   data = random_data_simp,
   gbm.x = 2:ncol(random_data_simp),
   gbm.y = 1,
-  offset = o,
+  # offset = o,
   family = "bernoulli",
   tree.complexity = tuned_param_2$interaction.depth[1],
   n.minobsinnode = tuned_param_2$n.minobsinnode[1],
   learning.rate = tuned_param_2$shrinkage[1],
   bag.fraction = tuned_param_2$bag.fraction[1],
   cv.folds = 10,
-  silent = FALSE
+  silent = FALSE,
+  plot.main = FALSE
 )
 
 save(brt_2,
-     file = paste0(path, "/brt_2.rData")
+  file = paste0(path, "/brt_2.rData")
 )
 
 ## 3.4 Get model stats ----
@@ -152,7 +160,7 @@ brt_2_stats <- calculate_brt_stats(model = brt_2) %>%
   rename(auc = discrimination.mean)
 
 save(brt_2_stats,
-     file = paste0(path, "/brt_2_stats.rData")
+  file = paste0(path, "/brt_2_stats.rData")
 )
 
 
@@ -163,7 +171,7 @@ save(brt_2_stats,
 brt_1_simp <- gbm.simplify(brt_1, plot = FALSE)
 
 save(brt_1_simp,
-     file = paste0(path, "/brt_1_simp.rData")
+  file = paste0(path, "/brt_1_simp.rData")
 )
 
 ## 4.2 Remove non-numeric characters from the row names ----
@@ -179,16 +187,22 @@ optimal_no_drops <- as.numeric(rownames(
 random_data_simp <- random_data %>%
   dplyr::select(PIWO_occ, brt_1_simp$pred.list[[optimal_no_drops[1]]])
 
+save(random_data_simp,
+  file = paste0(path, "/random_data_simp.rData")
+)
+
 ## 4.5 Tune parameters ----
 tune_data <- random_data_simp
-tune_data$offset <- o 
-tuned_param_3 <- tune_gbm_model(data = tune_data,
-                                formula = PIWO_occ ~ . +
-                                  offset(offset))
+# tune_data$offset <- o
 # tuned_param_3 <- tune_gbm_model(data = tune_data,
-#                                 formula = PIWO_occ ~ .)
+#                                 formula = PIWO_occ ~ . +
+#                                   offset(offset))
+tuned_param_3 <- tune_gbm_model(
+  data = tune_data,
+  formula = PIWO_occ ~ .
+)
 save(tuned_param_3,
-     file = paste0(path, "/tuned_param_3.rData")
+  file = paste0(path, "/tuned_param_3.rData")
 )
 
 ## 4.6 BRT Third Iteration ----
@@ -196,18 +210,19 @@ brt_3 <- gbm.step(
   data = random_data_simp,
   gbm.x = 2:ncol(random_data_simp),
   gbm.y = 1,
-  offset = o,
+  # offset = o,
   family = "bernoulli",
   tree.complexity = tuned_param_3$interaction.depth[1],
   n.minobsinnode = tuned_param_3$n.minobsinnode[1],
   learning.rate = tuned_param_3$shrinkage[1],
   bag.fraction = tuned_param_3$bag.fraction[1],
   cv.folds = 10,
-  silent = FALSE
+  silent = FALSE,
+  plot.main = FALSE
 )
 
 save(brt_3,
-     file = paste0(path, "/brt_3.rData")
+  file = paste0(path, "/brt_3.rData")
 )
 
 ## 4.7 Get model stats ----
@@ -215,13 +230,14 @@ brt_3_stats <- calculate_brt_stats(model = brt_3) %>%
   rename(auc = discrimination.mean)
 
 save(brt_3_stats,
-     file = paste0(path, "/brt_3_stats.rData")
+  file = paste0(path, "/brt_3_stats.rData")
 )
 
 # 5. View model stats ----
 brt_all_stats <- bind_rows(brt_1_stats, brt_2_stats, brt_3_stats)
+
 save(brt_all_stats,
-     file = paste0(path, "/brt_all_stats.rData")
+  file = paste0(path, "/brt_all_stats.rData")
 )
 
 # 4. Bootstrap model ----
@@ -231,19 +247,48 @@ save(brt_all_stats,
 # resources and needs.
 
 ## 4.1 Set up ----
-
 # add the offset back to the main dataframe
-random_data_simp$PIWO_offset <- o
+# random_data_simp$PIWO_offset <- o
 
-# Set the number of iterations 
+# Set the number of iterations
 n_iterations <- 100
 
 ## 4.2 run the bootstrap function ----
-bootstrap_models <- bootstrap_brt(random_data_simp, 
-                                  n_iterations, 
-                                  tuned_param_3)
+bootstrap_models <- bootstrap_brt(
+  random_data_simp,
+  n_iterations,
+  tuned_param_3
+)
 
 ## 4.3 Save models ----
 save(bootstrap_models,
   file = paste0(path, "/bootstrap_models.rData")
+)
+
+## 4.4 Summarize model stats ----
+# Assuming bootstrap_models$stats_df is your data frame
+stats_df <- bootstrap_models$stats_df[2:ncol(bootstrap_models$stats_df)]
+
+# Calculate the mean of each column
+mean_values <- colMeans(stats_df, na.rm = TRUE)
+# Calculate the standard deviation of each column
+sd_values <- apply(stats_df, 2, sd, na.rm = TRUE)
+
+# Combine the mean and standard deviation into a single data frame
+bootstrap_models_stats <- rbind(
+  mean = mean_values,
+  sd = sd_values
+  ) %>%
+  as.data.frame() %>%
+  dplyr::select(
+    deviance.mean,
+    correlation.mean,
+    discrimination.mean,
+    predict_AUC,
+    everything()
+  )
+
+# Save
+save(bootstrap_models_stats,
+  file = paste0(path, "/bootstrap_models_stats.rData")
 )
